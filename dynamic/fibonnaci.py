72387720b39bed0bf41e4d19e1af97e9f9a723a8
yujin-dev <qlcnal0211@gmail.com>
2021-04-26 23:58:29 +0900

    fibonnaci main 추가

--- dynamic/fibonnaci.py
+++ dynamic/fibonnaci.py
@@ -17,3 +17,28 @@ def fib_memoization(n, lookup):
         lookup[n] = fib(n-1, lookup) + fib(n-2, lookup)
         return lookup[n]
 
+
+if __name__ == '__main__':
+    userInput = int(input('Enter the number: '))
+
+    myList = [None for _ in range(userInput + 1)]
+
+    # base cases
+    myList[0] = 0
+    myList[1] = 1
+
+    startTime = time.time()
+    result = fibonacci(userInput)
+    stopTime = time.time()
+    print('Time:', (stopTime - startTime), 'Result:', result)
+
+    startTime = time.time()
+    result = fibonacciRec(userInput)
+    stopTime = time.time()
+    print('Time:', (stopTime - startTime), 'Result:', result)
+
+    startTime = time.time()
+    lookup = [None] * (101)
+    result = fib_memoization(userInput, lookup)
+    stopTime = time.time()
+    print('Time:', (stopTime - startTime), 'Result:', result)
